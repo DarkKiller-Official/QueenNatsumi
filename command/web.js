53ddd4eef0153b41464efa9a92f487df10379749
✰✶  CYBER DRAXO ✰✶ <99461944+CyberDraxo@users.noreply.github.com>
2022-05-08 11:46:51 +0530

    ❣️🔥 ```Command All Error Fix``` 🔥❣️

--- command/web.js
+++ command/web.js
@@ -3,7 +3,7 @@
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-WhatsAsena - Yusuf Usta
+WhatsAsena - Yusuf Usta */
 
 const Asena = require('../control');
 const {MessageType} = require('queen-natsumi-web-api');
